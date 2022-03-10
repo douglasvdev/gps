60d0db006b87756b4d15f3ca482c38afd03997c8
douglasvdev <douglasv.dev@gmail.com>
2022-03-10 15:37:50 -0300

    MVC Jogadores | Paginação, Buscador e alert para delete e API

---
+++ MVC/Controllers/JogadorController.cs
@@ -0,0 +1,216 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using API.Data;
+using API.Models;
+
+namespace MVC.Controllers
+{
+    public class JogadorController : Controller
+    {
+        #region CONSTRUTOR
+        private readonly Contexto _context;
+
+        public JogadorController(Contexto context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        #region VISUALIZAR
+
+        // GET: Jogador
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Jogadores.Where(j => j.Inativo == null).ToListAsync());
+        }
+
+        // GET: Jogador/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jogador = await _context.Jogadores
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (jogador == null)
+            {
+                return NotFound();
+            }
+
+            return View(jogador);
+        }
+
+        #endregion
+
+        #region CRIAR
+
+        // GET: Jogador/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Jogador/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,NomeJogador,Mensalista,ObsJogador,Inativo")] Jogador jogador)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(jogador);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(jogador);
+        }
+
+        #endregion
+
+        #region EDITAR
+
+        // GET: Jogador/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jogador = await _context.Jogadores.FindAsync(id);
+            if (jogador == null)
+            {
+                return NotFound();
+            }
+            return View(jogador);
+        }
+
+        // POST: Jogador/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,NomeJogador,Mensalista,ObsJogador,Inativo")] Jogador jogador)
+        {
+            if (id != jogador.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(jogador);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!JogadorExists(jogador.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(jogador);
+        }
+
+        #endregion
+
+        #region DELETAR (Desativado)
+
+        // GET: Jogador/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jogador = await _context.Jogadores
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (jogador == null)
+            {
+                return NotFound();
+            }
+
+            return View(jogador);
+        }
+
+        // POST: Jogador/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var jogador = await _context.Jogadores.FindAsync(id);
+            _context.Jogadores.Remove(jogador);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        #endregion
+
+        #region DELETAR
+
+        // GET: Conta/Edit/5
+        public async Task<IActionResult> Inativar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jogador = await _context.Jogadores.FindAsync(id);
+            jogador.Inativo = DateTime.Now;
+            if (jogador == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(jogador);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!JogadorExists(jogador.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                TempData["MsgSucesso"] = "Deletado com Sucesso";  //Transportar valor de MsgSucesso para função de alertify
+                return RedirectToAction(nameof(Index));
+            }
+            return View(jogador);
+
+        }
+
+        #endregion
+
+        private bool JogadorExists(int id)
+        {
+            return _context.Jogadores.Any(e => e.Id == id);
+        }
+    }
+}
