0d958bcaad90f1e8422712d5e76ee0ecfefe7eb8
douglasvdev <douglasv.dev@gmail.com>
2022-03-30 16:47:55 -0300

    View Tabela inserindo jogadores que participaram da partida alterando o parametro (V, D e E)

---
+++ API/Models/Tabela.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Models
+{
+    public class Tabela
+    {
+        public Scout scout { get; set; }
+        public Times times { get; set; }
+    }
+}
--- MVC/Controllers/ScoutController.cs
+++ MVC/Controllers/ScoutController.cs
@@ -132,28 +132,456 @@ namespace MVC.Controllers
 
             //if (ModelState.IsValid)
             //{
+            #region Jogadores Time A
+            List<int> listaJogTimeA = new List<int>();
+            listaJogTimeA.Add(scout.JogadorIdA1 = jogA1);
+            //listaJogTimeA.Add(0);
+
+            listaJogTimeA.Add((int)(scout.JogadorIdA2 = jogA2));
+
+            listaJogTimeA.Add((int)(scout.JogadorIdA3 = jogA3));
+
+            listaJogTimeA.Add((int)(scout.JogadorIdA4 = jogA4));
+
+            listaJogTimeA.Add((int)(scout.JogadorIdA5 = jogA5));
+
+            listaJogTimeA.Add((int)(scout.JogadorIdA6 = jogA6));
+
+            listaJogTimeA.Add((int)(scout.JogadorIdA7 = jogA7));
+
+            listaJogTimeA.Add((int)(scout.JogadorIdA8 = jogA8));
+
+            listaJogTimeA.Add((int)(scout.JogadorIdA9 = jogA9));
+
+            listaJogTimeA.Add((int)(scout.JogadorIdA10 = jogA10));
+            #endregion
+
+            #region Gols Time A
+            List<int> listaGolTimeA = new List<int>();
+            listaGolTimeA.Add((int)(scout.GolsA1 = golA1));
+
+            listaGolTimeA.Add((int)(scout.GolsA2 = golA2));
+
+            listaGolTimeA.Add((int)(scout.GolsA3 = golA3));
+
+            listaGolTimeA.Add((int)(scout.GolsA4 = golA4));
+
+            listaGolTimeA.Add((int)(scout.GolsA5 = golA5));
+
+            listaGolTimeA.Add((int)(scout.GolsA6 = golA6));
+
+            listaGolTimeA.Add((int)(scout.GolsA7 = golA7));
+
+            listaGolTimeA.Add((int)(scout.GolsA8 = golA8));
+
+            listaGolTimeA.Add((int)(scout.GolsA9 = golA9));
+
+            listaGolTimeA.Add((int)(scout.GolsA10 = golA10));
+
+            #endregion
+
+            #region Jogadores Time B
+            List<int> listaJogTimeB = new List<int>();
+            listaJogTimeB.Add(scout.JogadorIdB1 = jogB1);
+            //listaJogTimeB.Add(0);
+
+            listaJogTimeB.Add((int)(scout.JogadorIdB2 = jogB2));
+
+            listaJogTimeB.Add((int)(scout.JogadorIdB3 = jogB3));
+
+            listaJogTimeB.Add((int)(scout.JogadorIdB4 = jogB4));
+
+            listaJogTimeB.Add((int)(scout.JogadorIdB5 = jogB5));
+
+            listaJogTimeB.Add((int)(scout.JogadorIdB6 = jogB6));
+
+            listaJogTimeB.Add((int)(scout.JogadorIdB7 = jogB7));
+
+            listaJogTimeB.Add((int)(scout.JogadorIdB8 = jogB8));
+
+            listaJogTimeB.Add((int)(scout.JogadorIdB9 = jogB9));
+
+            listaJogTimeB.Add((int)(scout.JogadorIdB10 = jogB10));
+            #endregion
+
+            #region Gols Time B
+            List<int> listaGolTimeB = new List<int>();
+            listaGolTimeB.Add((int)(scout.GolsB1 = golB1));
+
+            listaGolTimeB.Add((int)(scout.GolsB2 = golB2));
+
+            listaGolTimeB.Add((int)(scout.GolsB3 = golB3));
+
+            listaGolTimeB.Add((int)(scout.GolsB4 = golB4));
+
+            listaGolTimeB.Add((int)(scout.GolsB5 = golB5));
+
+            listaGolTimeB.Add((int)(scout.GolsB6 = golB6));
+
+            listaGolTimeB.Add((int)(scout.GolsB7 = golB7));
+
+            listaGolTimeB.Add((int)(scout.GolsB8 = golB8));
+
+            listaGolTimeB.Add((int)(scout.GolsB9 = golB9));
+
+            listaGolTimeB.Add((int)(scout.GolsB10 = golB10));
+
+            #endregion
+
+            #region Regra de Negocio
+
             if (GolA > GolB)
             {
-                scout.JogadorId = jogA1;
-                scout.Presente = true;
-                scout.ParametroId = 3;
-                scout.Gol = golA1;
-                _context.Add(scout);
-                await _context.SaveChangesAsync();
+                for (int i = 0; i < listaJogTimeA.Count; i++)
+                {
+                    if (listaJogTimeA[i] != 0)
+                    {
+                        scout.Id = 0;
+                        scout.DtPartida = DtPartida;
+                        scout.JogadorId = listaJogTimeA[i];
+                        scout.Presente = true;
+                        scout.ParametroId = 3;
+                        scout.Gol = listaGolTimeA[i];
+                        scout.ObsScout = ObsScout;
+                        _context.Add(scout);
+                        await _context.SaveChangesAsync();
+                    }
 
-                scout.Id = 0;
-                scout.JogadorId = jogB1;
-                scout.Presente = true;
-                scout.ParametroId = 4;
-                scout.Gol = golB1;
-                _context.Add(scout);
-                await _context.SaveChangesAsync();
+                    if (listaJogTimeB[i] != 0)
+                    {
+                        scout.Id = 0;
+                        scout.DtPartida = DtPartida;
+                        scout.JogadorId = listaJogTimeB[i];
+                        scout.Presente = true;
+                        scout.ParametroId = 4;
+                        scout.Gol = listaGolTimeB[i];
+                        scout.ObsScout = ObsScout;
+                        _context.Add(scout);
+                        await _context.SaveChangesAsync();
+                    }
+                }
+            } 
+            else if (GolA < GolB)
+            {
+                for (int i = 0; i < listaJogTimeA.Count; i++)
+                {
+                    if (listaJogTimeA[i] != 0)
+                    {
+                        scout.Id = 0;
+                        scout.DtPartida = DtPartida;
+                        scout.JogadorId = listaJogTimeA[i];
+                        scout.Presente = true;
+                        scout.ParametroId = 4;
+                        scout.Gol = listaGolTimeA[i];
+                        scout.ObsScout = ObsScout;
+                        _context.Add(scout);
+                        await _context.SaveChangesAsync();
+                    }
 
-                //return RedirectToAction(nameof(Index));
+                    if (listaJogTimeB[i] != 0)
+                    {
+                        scout.Id = 0;
+                        scout.DtPartida = DtPartida;
+                        scout.JogadorId = listaJogTimeB[i];
+                        scout.Presente = true;
+                        scout.ParametroId = 3;
+                        scout.Gol = listaGolTimeB[i];
+                        scout.ObsScout = ObsScout;
+                        _context.Add(scout);
+                        await _context.SaveChangesAsync();
+                    }
+                }
             }
+            else
+            {
+                for (int i = 0; i < listaJogTimeA.Count; i++)
+                {
+                    if (listaJogTimeA[i] != 0)
+                    {
+                        scout.Id = 0;
+                        scout.DtPartida = DtPartida;
+                        scout.JogadorId = listaJogTimeA[i];
+                        scout.Presente = true;
+                        scout.ParametroId = 5;
+                        scout.Gol = listaGolTimeA[i];
+                        scout.ObsScout = ObsScout;
+                        _context.Add(scout);
+                        await _context.SaveChangesAsync();
+                    }
+
+                    if (listaJogTimeB[i] != 0)
+                    {
+                        scout.Id = 0;
+                        scout.DtPartida = DtPartida;
+                        scout.JogadorId = listaJogTimeB[i];
+                        scout.Presente = true;
+                        scout.ParametroId = 5;
+                        scout.Gol = listaGolTimeB[i];
+                        scout.ObsScout = ObsScout;
+                        _context.Add(scout);
+                        await _context.SaveChangesAsync();
+                    }
+                }
+            }
+            
+            #endregion
             //}
-            //ViewData["JogadorId"] = new SelectList(_context.Jogadores, "Id", "Mensalista", scout.JogadorId);
-            //ViewData["ParametroId"] = new SelectList(_context.Parametros, "Id", "DescParametro", scout.ParametroId);
+
+            return RedirectToAction(nameof(Tabela));
+        }
+
+        #endregion
+
+        #region PARTIDA Apagar
+
+        public IActionResult Partida()
+        {
+            ViewData["JogadorIdA1"] = new SelectList(_context.Jogadores.Where(j => j.Inativo == null), "Id", "NomeJogador");
+            ViewData["JogadorIdA2"] = new SelectList(_context.Jogadores.Where(j => j.Inativo == null), "Id", "NomeJogador");
+            ViewData["JogadorIdA3"] = new SelectList(_context.Jogadores.Where(j => j.Inativo == null), "Id", "NomeJogador");
+            ViewData["JogadorIdA4"] = new SelectList(_context.Jogadores.Where(j => j.Inativo == null), "Id", "NomeJogador");
+            ViewData["JogadorIdA5"] = new SelectList(_context.Jogadores.Where(j => j.Inativo == null), "Id", "NomeJogador");
+            ViewData["JogadorIdA6"] = new SelectList(_context.Jogadores.Where(j => j.Inativo == null), "Id", "NomeJogador");
+            ViewData["JogadorIdA7"] = new SelectList(_context.Jogadores.Where(j => j.Inativo == null), "Id", "NomeJogador");
+            ViewData["JogadorIdA8"] = new SelectList(_context.Jogadores.Where(j => j.Inativo == null), "Id", "NomeJogador");
+            ViewData["JogadorIdA9"] = new SelectList(_context.Jogadores.Where(j => j.Inativo == null), "Id", "NomeJogador");
+            ViewData["JogadorIdA10"] = new SelectList(_context.Jogadores.Where(j => j.Inativo == null), "Id", "NomeJogador");
+
+            ViewData["JogadorIdB1"] = new SelectList(_context.Jogadores.Where(j => j.Inativo == null), "Id", "NomeJogador");
+            ViewData["JogadorIdB2"] = new SelectList(_context.Jogadores.Where(j => j.Inativo == null), "Id", "NomeJogador");
+            ViewData["JogadorIdB3"] = new SelectList(_context.Jogadores.Where(j => j.Inativo == null), "Id", "NomeJogador");
+            ViewData["JogadorIdB4"] = new SelectList(_context.Jogadores.Where(j => j.Inativo == null), "Id", "NomeJogador");
+            ViewData["JogadorIdB5"] = new SelectList(_context.Jogadores.Where(j => j.Inativo == null), "Id", "NomeJogador");
+            ViewData["JogadorIdB6"] = new SelectList(_context.Jogadores.Where(j => j.Inativo == null), "Id", "NomeJogador");
+            ViewData["JogadorIdB7"] = new SelectList(_context.Jogadores.Where(j => j.Inativo == null), "Id", "NomeJogador");
+            ViewData["JogadorIdB8"] = new SelectList(_context.Jogadores.Where(j => j.Inativo == null), "Id", "NomeJogador");
+            ViewData["JogadorIdB9"] = new SelectList(_context.Jogadores.Where(j => j.Inativo == null), "Id", "NomeJogador");
+            ViewData["JogadorIdB10"] = new SelectList(_context.Jogadores.Where(j => j.Inativo == null), "Id", "NomeJogador");
+
+            //ViewData["ParametroId"] = new SelectList(_context.Parametros, "Id", "DescParametro");
+            return View();
+        }
+
+        public async Task<ActionResult> Partida(DateTime DtPartida, String ObsScout,
+            int jogA1, int golA1,
+            int jogA2, int golA2,
+            int jogA3, int golA3,
+            int jogA4, int golA4,
+            int jogA5, int golA5,
+            int jogA6, int golA6,
+            int jogA7, int golA7,
+            int jogA8, int golA8,
+            int jogA9, int golA9,
+            int jogA10, int golA10,
+            int jogB1, int golB1,
+            int jogB2, int golB2,
+            int jogB3, int golB3,
+            int jogB4, int golB4,
+            int jogB5, int golB5,
+            int jogB6, int golB6,
+            int jogB7, int golB7,
+            int jogB8, int golB8,
+            int jogB9, int golB9,
+            int jogB10, int golB10, Scout scout)
+        {
+            var GolA = golA1 + golA2 + golA3 + golA4 + golA5 + golA6 + golA7 + golA8 + golA9 + golA10;
+            var GolB = golB1 + golB2 + golB3 + golB4 + golB5 + golB6 + golB7 + golB8 + golB9 + golB10;
+                        
+            //Tabela partida = new Tabela()
+            //{
+            //    scout = scout,
+            //    times = equipes
+            //};
+            #region Jogadores Time A
+            List<int> listaJogTimeA = new List<int>();
+            scout.JogadorIdA1 = jogA1;
+            listaJogTimeA.Add(0);
+
+            scout.JogadorIdA2 = jogA2;
+            listaJogTimeA.Add(1);
+
+            scout.JogadorIdA3 = jogA3;
+            listaJogTimeA.Add(2);
+
+            scout.JogadorIdA4 = jogA4;
+            listaJogTimeA.Add(3);
+
+            scout.JogadorIdA5 = jogA5;
+            listaJogTimeA.Add(4);
+
+            scout.JogadorIdA6 = jogA6;
+            listaJogTimeA.Add(5);
+
+            scout.JogadorIdA7 = jogA7;
+            listaJogTimeA.Add(6);
+
+            scout.JogadorIdA8 = jogA8;
+            listaJogTimeA.Add(7);
+
+            scout.JogadorIdA9 = jogA9;
+            listaJogTimeA.Add(8);
+
+            scout.JogadorIdA10 = jogA10;
+            listaJogTimeA.Add(9);
+            #endregion
+
+            #region Gols Time A
+            List<int> listaGolTimeA = new List<int>();
+            if (golA1.ToString() == null)
+            {
+                scout.GolsA1 = 0;
+                listaGolTimeA.Add(0);
+            }
+            else
+            {
+                scout.GolsA1 = golA1;
+                listaGolTimeA.Add(0);
+            }
+
+            if (golA2.ToString() == null)
+            {
+                scout.GolsA2 = 0;
+                listaGolTimeA.Add(1);
+            }
+            else
+            {
+                scout.GolsA2 = golA2;
+                listaGolTimeA.Add(1);
+            }
+
+            if (golA3.ToString() == null)
+            {
+                scout.GolsA3 = 0;
+                listaGolTimeA.Add(2);
+            }
+            else
+            {
+                scout.GolsA3 = golA3;
+                listaGolTimeA.Add(2);
+            }
+
+            if (golA4.ToString() == null)
+            {
+                scout.GolsA4 = 0;
+                listaGolTimeA.Add(3);
+            }
+            else
+            {
+                scout.GolsA4 = golA4;
+                listaGolTimeA.Add(3);
+            }
+
+            if (golA5.ToString() == null)
+            {
+                scout.GolsA5 = 0;
+                listaGolTimeA.Add(4);
+            }
+            else
+            {
+                scout.GolsA5 = golA5;
+                listaGolTimeA.Add(4);
+            }
+
+            if (golA6.ToString() == null)
+            {
+                scout.GolsA6 = 0;
+                listaGolTimeA.Add(5);
+            }
+            else
+            {
+                scout.GolsA6 = golA6;
+                listaGolTimeA.Add(5);
+            }
+
+            if (golA7.ToString() == null)
+            {
+                scout.GolsA7 = 0;
+                listaGolTimeA.Add(6);
+            }
+            else
+            {
+                scout.GolsA7 = golA7;
+                listaGolTimeA.Add(6);
+            }
+
+            if (golA8.ToString() == null)
+            {
+                scout.GolsA8 = 0;
+                listaGolTimeA.Add(7);
+            }
+            else
+            {
+                scout.GolsA8 = golA8;
+                listaGolTimeA.Add(7);
+            }
+
+            if (golA9.ToString() == null)
+            {
+                scout.GolsA9 = 0;
+                listaGolTimeA.Add(8);
+            }
+            else
+            {
+                scout.GolsA9 = golA9;
+                listaGolTimeA.Add(8);
+            }
+
+            if (golA10.ToString() == null)
+            {
+                scout.GolsA10 = 0;
+                listaGolTimeA.Add(9);
+            }
+            else
+            {
+                scout.GolsA10 = golA10;
+                listaGolTimeA.Add(9);
+            }
+
+            #endregion
+
+            if (GolA > GolB)
+            {
+                for (int i = 0; i < listaJogTimeA.Count; i++)
+                {
+                    scout.DtPartida = DtPartida;
+                    scout.JogadorId = listaJogTimeA[i];
+                    scout.Presente = true;
+                    scout.ParametroId = 3;
+                    scout.Gol = listaGolTimeA[i];
+                    scout.ObsScout = ObsScout;
+                    _context.Add(scout);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            if (GolA < GolB)
+            {
+                for (int i = 0; i < listaJogTimeA.Count; i++)
+                {
+                    scout.DtPartida = DtPartida;
+                    scout.JogadorId = listaJogTimeA[i];
+                    scout.Presente = true;
+                    scout.ParametroId = 4;
+                    scout.Gol = listaGolTimeA[i];
+                    scout.ObsScout = ObsScout;
+                    _context.Add(scout);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            else
+            {
+                for (int i = 0; i < listaJogTimeA.Count; i++)
+                {
+                    scout.DtPartida = DtPartida;
+                    scout.JogadorId = listaJogTimeA[i];
+                    scout.Presente = true;
+                    scout.ParametroId = 5;
+                    scout.Gol = listaGolTimeA[i];
+                    scout.ObsScout = ObsScout;
+                    _context.Add(scout);
+                    await _context.SaveChangesAsync();
+                }
+            }
+
             return RedirectToAction(nameof(Index));
         }
 
