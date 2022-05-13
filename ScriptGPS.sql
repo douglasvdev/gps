9ed276212ef49bbcdca9e71361a0bae167ad2826
douglasvdev <douglasv.dev@gmail.com>
2022-05-13 19:03:55 -0300

    Modelo de procedure

--- ScriptGPS.sql
+++ ScriptGPS.sql
@@ -4,6 +4,18 @@ select *
 where routine_type = 'PROCEDURE'
 */
 
+/*
+SELECT A.NAME, A.TYPE, B.TEXT
+  FROM SYSOBJECTS  A (nolock)
+  JOIN SYSCOMMENTS B (nolock) 
+    ON A.ID = B.ID
+WHERE B.TEXT LIKE '%SELECT DISTINCT%'  --- Informação a ser procurada no corpo da procedure, funcao ou view
+  AND A.TYPE = 'P'                     --- Tipo de objeto a ser localizado no caso procedure
+ ORDER BY A.NAME
+ */
+
+
+
 --DECLARE @SEMANA INT , @FIM BIT
 --SELECT @SEMANA = 3595,  @FIM = 1
 
